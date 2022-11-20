c160e41aa7cd0241cc2ed9851dd2911d67748447
first-portfolio-igorv <102622018+first-portfolio-igorv@users.noreply.github.com>
2022-11-20 20:58:08 +0200

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CensorComponent } from './censor/censor.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CensorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
